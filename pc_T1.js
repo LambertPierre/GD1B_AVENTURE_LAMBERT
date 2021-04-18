14ac3893523a38633bd32933b626c65778efda0a
LambertPierre <74670133+LambertPierre@users.noreply.github.com>
2021-04-18 13:09:12 +0200

    go to Angers

--- pc_T1.js
+++ pc_T1.js
@@ -1,13 +1,12 @@
 var player;
-var stars;
-var bombs;
-var platforms;
+var epee;
 var cursors;
-var score = 0;
 var gameOver = false;
-var scoreText;
 
-var speed = 10;
+var straf= false;
+var direction = 'bb';
+
+var epeeCD = true;
 
 var soulCollected = false;
 
@@ -30,6 +29,9 @@ class pc_T1 extends Phaser.Scene{
 
         this.load.spritesheet('pnj', 'assets/items/pnj_sprite.png', { frameWidth: 200, frameHeight: 300 });
         this.load.spritesheet('pnjApp', 'assets/items/pnj_apparition_sprite.png', { frameWidth: 300, frameHeight: 400 });
+
+        this.load.spritesheet('epee', 'assets/placeholder/epee.png', { frameWidth: 300, frameHeight: 200 });
+        this.load.spritesheet('ennemi', 'assets/placeholder/phe_deplacement.png', { frameWidth: 100, frameHeight: 150 });
     }
 
     create ()
@@ -43,7 +45,7 @@ class pc_T1 extends Phaser.Scene{
         
         
         player = this.physics.add.sprite(700, 700, 'dude');
-        player.setCollideWorldBounds(true);
+        //player.setCollideWorldBounds(true);
         player.body.height = 100;
         player.body.setOffset(0, 50);   
 
@@ -145,7 +147,8 @@ class pc_T1 extends Phaser.Scene{
             {up:Phaser.Input.Keyboard.KeyCodes.Z,
             down:Phaser.Input.Keyboard.KeyCodes.S,
             left:Phaser.Input.Keyboard.KeyCodes.Q,
-            right:Phaser.Input.Keyboard.KeyCodes.D});
+            right:Phaser.Input.Keyboard.KeyCodes.D,
+            epeeInput:Phaser.Input.Keyboard.KeyCodes.SHIFT});
 
 
     }
@@ -171,55 +174,195 @@ class pc_T1 extends Phaser.Scene{
 
         }*/
         //------------------------------------------------------------------- Déplacement ----------------------------------------------------------------//
-
-        if (cursors.right.isDown && cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown){
-            player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale haut doite                     
-            player.y -= speed/Math.sqrt(2); 
+        /*if (cursors.up.isDown && cursors.left.isDown){
+            player.setVelocityX(-150);
+            player.setVelocityY(-150);
+            player.anims.play('phphg', true);
+        }
+        else if (cursors.up.isUp && cursors.left.isUp){
+            player.setVelocityX(0);
+            player.setVelocityY(0);
+        }
+        else if (cursors.up.isDown && cursors.right.isDown){
+            player.setVelocityX(150);
+            player.setVelocityY(-150);
             player.anims.play('phphd', true);
+        }
+        else if (cursors.up.isUp && cursors.right.isUp){
+            player.setVelocityX(0);
+            player.setVelocityY(0);
+        }
+        else if (cursors.right.isDown){
+            player.setVelocityX(300);
+            player.anims.play('phpdd', true);
+        }
+        else if (cursors.left.isDown){
+            player.setVelocityX(-300);
+            player.anims.play('phpgg', true);
+        }
+        else if (cursors.right.isUp && cursors.left.isUp){
+            player.setVelocityX(0);
+        }
+        else if (cursors.up.isDown){
+            player.setVelocityY(-300);
+            player.anims.play('phphh', true);
+        }
+        else if (cursors.down.isDown){
+            player.setVelocityY(300);
+            player.anims.play('phpbb', true);
+        }
+        else if (cursors.up.isUp && cursors.down.isUp){
+            player.setVelocityY(0);
+        }*/
+
+        
+        
+        if (cursors.right.isDown && cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown){
+            /*player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale haut doite                     
+            player.y -= speed/Math.sqrt(2); */
+            player.setVelocityX(300);
+            player.setVelocityY(-300);
+            player.anims.play('phphd', true);
+            direction = 'hd';
         }
 
         else if (cursors.left.isDown && cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown){
-            player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale haut gauche                      
-            player.y -= speed/Math.sqrt(2); 
-            player.anims.play('phphg', true);                      
+            /*player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale haut gauche                      
+            player.y -= speed/Math.sqrt(2); */
+            player.setVelocityX(-300);
+            player.setVelocityY(-300);
+            player.anims.play('phphg', true);
+            direction = 'hg';                      
         }
 
         else if (cursors.down.isDown && cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
-            player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale bas gauche                      
-            player.y += speed/Math.sqrt(2);
-            player.anims.play('phpbg', true);                       
+            /*player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale bas gauche                      
+            player.y += speed/Math.sqrt(2);*/
+            player.setVelocityX(-300);
+            player.setVelocityY(300);
+            player.anims.play('phpbg', true);
+            direction = 'bg';                       
         }
 
         else if (cursors.down.isDown && cursors.right.isDown && !cursors.left.isDown && !cursors.up.isDown){
-            player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale bas droite                      
-            player.y += speed/Math.sqrt(2);
-            player.anims.play('phpbd', true);                         
+            /*player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale bas droite                      
+            player.y += speed/Math.sqrt(2);*/
+            player.setVelocityX(300);
+            player.setVelocityY(300);
+            player.anims.play('phpbd', true);
+            direction = 'bd';                         
         }
 
         // -- Déplacements Horizontaux Verticaux --
 
 
         else if (cursors.right.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.up.isDown){
-            player.x += speed;
+            /*player.x += speed;*/
+            player.setVelocityX(400);
+            player.setVelocityY(0);
             player.anims.play('phpdd', true);                        // Déplacement vers la droite
+            direction = 'dd';
         }
 
         else if (cursors.left.isDown && !cursors.right.isDown && !cursors.down.isDown && !cursors.up.isDown){
-            player.x -= speed;
+            /*player.x -= speed;*/
+            player.setVelocityX(-400);
+            player.setVelocityY(0);
             player.anims.play('phpgg', true);                        // Déplacement vers la gauche
+            direction = 'gg';
         }
 
         else if (cursors.down.isDown && !cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
-            player.y += speed;
+            /*player.y += speed;*/
+            player.setVelocityY(400);
+            player.setVelocityX(0);
             player.anims.play('phpbb', true);                        // Déplacement vers le bas
+            direction = 'bb';
         }
 
         else if (cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.right.isDown){
-            player.y -= speed;
+            /*player.y -= speed;*/
+            player.setVelocityY(-400);
+            player.setVelocityX(0);
             player.anims.play('phphh', true);                        // Déplacement vers le haut
+            direction = 'hh';
         }
         else{
+            player.setVelocityX(0);
+            player.setVelocityY(0);
             player.anims.play('phpbb',false);
+        }
+
+        // ------------------------------------------------------ POUVOIRS -------------------------------------------------------- //
+
+        if (cursors.epeeInput.isDown && epeeCD==true){
+            if (direction=='hd'){
+                epee = this.physics.add.sprite(player.x+100,player.y-100, 'epee').setRotation(0.735);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='hg'){
+                epee = this.physics.add.sprite(player.x-100,player.y-100, 'epee').setRotation(-0.735);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bd'){
+                epee = this.physics.add.sprite(player.x+100,player.y+100, 'epee').setRotation(2.305);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bg'){
+                epee = this.physics.add.sprite(player.x-100,player.y+100, 'epee').setRotation(-2.305);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='dd'){
+                epee = this.physics.add.sprite(player.x+150,player.y, 'epee').setRotation(1.57);
+                epee.body.height = 300;
+                epee.body.width = 200;
+                epee.body.setOffset(50, -50);   
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='gg'){
+                epee = this.physics.add.sprite(player.x-150,player.y, 'epee').setRotation(-1.57);
+                epee.body.height = 300;
+                epee.body.width = 200;
+                epee.body.setOffset(50, -50);  
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bb'){
+                epee = this.physics.add.sprite(player.x,player.y+150, 'epee').setRotation(-3.14);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='hh'){
+                epee = this.physics.add.sprite(player.x,player.y-150, 'epee');
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+
         }
 
         if (player.y<100){
--- pc_T2.js
+++ pc_T2.js
@@ -1,17 +1,19 @@
-var player;
-var stars;
-var bombs;
+
 var platforms;
 var cursors;
-var score = 0;
-var gameOver = false;
-var scoreText;
 
-var speed = 10;
+
+var gameOver = false;
+
+var pnj;
+var pnjApp;
+var playerApproach = false;
 
 var soulCollected = false;
 
 var playerApproach = false;
+
+var ennemiAggro = false;
 
 class pc_T2 extends Phaser.Scene{
     constructor(){
@@ -30,6 +32,9 @@ class pc_T2 extends Phaser.Scene{
 
         this.load.spritesheet('pnj', 'assets/items/pnj_sprite.png', { frameWidth: 200, frameHeight: 300 });
         this.load.spritesheet('pnjApp', 'assets/items/pnj_apparition_sprite.png', { frameWidth: 300, frameHeight: 400 });
+
+        this.load.spritesheet('epee', 'assets/placeholder/epee.png', { frameWidth: 300, frameHeight: 200 });
+        this.load.spritesheet('ennemi', 'assets/placeholder/phe_deplacement.png', { frameWidth: 100, frameHeight: 150 });
     }
 
     create ()
@@ -41,6 +46,7 @@ class pc_T2 extends Phaser.Scene{
         const vert = map.createStaticLayer('vert', tileset, 0, 0);
         const orange = map.createStaticLayer('orange', tileset, 0, 0);
         orange.setCollisionByExclusion(-1,true);
+        bleu.setCollisionByExclusion(-1,true);
         
         
         player = this.physics.add.sprite(1300, 1800, 'dude');
@@ -49,86 +55,18 @@ class pc_T2 extends Phaser.Scene{
         player.body.setOffset(0, 50);   
 
         this.physics.add.collider(orange, player);
+        this.physics.add.collider(bleu, player);
         const jaune = map.createStaticLayer('jaune', tileset, 0, 0);
 
-        this.cameras.main.setBounds(0, 0, 1920, 2160);
+        this.cameras.main.setBounds(0, 0, 2500, 2000);
         this.cameras.main.startFollow(player);
         this.cameras.main.setZoom(1);
     
         //   soul = this.add.sprite(400, 300, 'soul');
 
+        ennemi = this.physics.add.sprite(1200, 800, 'ennemi');
 
         //----------------------------------------------------------------  ANIMATION  -------------------------------------------------------------------------//
-        /*this.anims.create({
-            key: 'phpbb',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 1 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phpbd',
-            frames: this.anims.generateFrameNumbers('dude', { start: 2, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phpdd',
-            frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 5 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phphd',
-            frames: this.anims.generateFrameNumbers('dude', { start: 6, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phphh',
-            frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 9 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phphg',
-            frames: this.anims.generateFrameNumbers('dude', { start: 10, end: 11 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phpgg',
-            frames: this.anims.generateFrameNumbers('dude', { start: 12, end: 13 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'phpbg',
-            frames: this.anims.generateFrameNumbers('dude', { start: 14, end: 15 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        //----------------------------------------------------------------------//
-
-        this.anims.create({
-            key: 'soul_anim',
-            frames: this.anims.generateFrameNumbers('soul', { start: 0, end: 12 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'pnj_anim',
-            frames: this.anims.generateFrameNumbers('pnj', { start: 0, end: 6 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'pnjApp_anim',
-            frames: this.anims.generateFrameNumbers('pnjApp', { start: 0, end: 4 }),
-            frameRate: 10,
-            repeat: 0
-        });*/
 
         //--------------------------------------------------------------  FIN ANIMATION  ------------------------------------------------------------------------//
 
@@ -138,7 +76,8 @@ class pc_T2 extends Phaser.Scene{
             {up:Phaser.Input.Keyboard.KeyCodes.Z,
             down:Phaser.Input.Keyboard.KeyCodes.S,
             left:Phaser.Input.Keyboard.KeyCodes.Q,
-            right:Phaser.Input.Keyboard.KeyCodes.D});
+            right:Phaser.Input.Keyboard.KeyCodes.D,
+            epeeInput:Phaser.Input.Keyboard.KeyCodes.SHIFT});
 
 
     }
@@ -153,53 +92,159 @@ class pc_T2 extends Phaser.Scene{
         //------------------------------------------------------------------- Déplacement ----------------------------------------------------------------//
 
         if (cursors.right.isDown && cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown){
-            player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale haut doite                     
-            player.y -= speed/Math.sqrt(2); 
+            // Déplacements vers la diagonale haut doite                     
+            player.setVelocityX(300);
+            player.setVelocityY(-300);
             player.anims.play('phphd', true);
+            direction = 'hd';
         }
 
         else if (cursors.left.isDown && cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown){
-            player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale haut gauche                      
-            player.y -= speed/Math.sqrt(2); 
-            player.anims.play('phphg', true);                      
+            // Déplacements vers la diagonale haut gauche                      
+            player.setVelocityX(-300);
+            player.setVelocityY(-300);
+            player.anims.play('phphg', true);
+            direction = 'hg';                      
         }
 
-        else if (cursors.down.isDown && cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
-            player.x -= speed/Math.sqrt(2);           // Déplacements vers la diagonale bas gauche                      
-            player.y += speed/Math.sqrt(2);
-            player.anims.play('phpbg', true);                       
+        else if (cursors.down.isDown && cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){          
+            // Déplacements vers la diagonale bas gauche                      
+            player.setVelocityX(-300);
+            player.setVelocityY(300);
+            player.anims.play('phpbg', true);
+            direction = 'bg';                       
         }
 
         else if (cursors.down.isDown && cursors.right.isDown && !cursors.left.isDown && !cursors.up.isDown){
-            player.x += speed/Math.sqrt(2);           // Déplacements vers la diagonale bas droite                      
-            player.y += speed/Math.sqrt(2);
-            player.anims.play('phpbd', true);                         
+        // Déplacements vers la diagonale bas droite                      
+            player.setVelocityX(300);
+            player.setVelocityY(300);
+            player.anims.play('phpbd', true);
+            direction = 'bd';                         
         }
 
         // -- Déplacements Horizontaux Verticaux --
 
 
         else if (cursors.right.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.up.isDown){
-            player.x += speed;
+            player.setVelocityX(400);
+            player.setVelocityY(0);
             player.anims.play('phpdd', true);                        // Déplacement vers la droite
+            direction = 'dd';
         }
 
         else if (cursors.left.isDown && !cursors.right.isDown && !cursors.down.isDown && !cursors.up.isDown){
-            player.x -= speed;
+            player.setVelocityX(-400);
+            player.setVelocityY(0);
             player.anims.play('phpgg', true);                        // Déplacement vers la gauche
+            direction = 'gg';
         }
 
         else if (cursors.down.isDown && !cursors.left.isDown && !cursors.up.isDown && !cursors.right.isDown){
-            player.y += speed;
+            player.setVelocityY(400);
+            player.setVelocityX(0);
             player.anims.play('phpbb', true);                        // Déplacement vers le bas
+            direction = 'bb';
         }
 
         else if (cursors.up.isDown && !cursors.left.isDown && !cursors.down.isDown && !cursors.right.isDown){
-            player.y -= speed;
+            player.setVelocityY(-400);
+            player.setVelocityX(0);
             player.anims.play('phphh', true);                        // Déplacement vers le haut
+            direction = 'hh';
         }
         else{
+            player.setVelocityX(0);
+            player.setVelocityY(0);
             player.anims.play('phpbb',false);
+        }
+        // ------------------------------------------------------- EVENEMENT ------------------------------------------------------ //
+
+        if (player.y<1100 && playerApproach == false){
+            playerApproach = true;
+            pnj = this.add.sprite(1300, 700, 'pnj');
+            pnj.anims.play('pnj_anim', true);
+            pnjApp = this.add.sprite(1300, 700, 'pnjApp').setScale(1.5);
+            pnjApp.anims.play('pnjApp_anim', true);
+            setTimeout(function(){pnjApp.destroy()}, 500);
+        }
+        // ---------- Aggro ----------- //
+
+        if (){
+            
+        }
+
+
+        // ------------------------------------------------------ POUVOIRS -------------------------------------------------------- //
+
+        if (cursors.epeeInput.isDown && epeeCD==true){
+            if (direction=='hd'){
+                epee = this.physics.add.sprite(player.x+100,player.y-100, 'epee').setRotation(0.735);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='hg'){
+                epee = this.physics.add.sprite(player.x-100,player.y-100, 'epee').setRotation(-0.735);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bd'){
+                epee = this.physics.add.sprite(player.x+100,player.y+100, 'epee').setRotation(2.305);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bg'){
+                epee = this.physics.add.sprite(player.x-100,player.y+100, 'epee').setRotation(-2.305);
+                epee.body.height = 250;
+                epee.body.width = 250;
+                epee.body.setOffset(25, -25);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='dd'){
+                epee = this.physics.add.sprite(player.x+150,player.y, 'epee').setRotation(1.57);
+                epee.body.height = 300;
+                epee.body.width = 200;
+                epee.body.setOffset(50, -50);   
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='gg'){
+                epee = this.physics.add.sprite(player.x-150,player.y, 'epee').setRotation(-1.57);
+                epee.body.height = 300;
+                epee.body.width = 200;
+                epee.body.setOffset(50, -50);  
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='bb'){
+                epee = this.physics.add.sprite(player.x,player.y+150, 'epee').setRotation(-3.14);
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+            if (direction=='hh'){
+                epee = this.physics.add.sprite(player.x,player.y-150, 'epee');
+                epeeCD=false;
+                setTimeout(function(){epeeCD=true}, 1000);
+                setTimeout(function(){epee.destroy()}, 300);
+            }
+
         }
 
         
